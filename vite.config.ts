bac5659109c3d08d1b523096dcf7278d8fa98fbf
Dan Valinotti <dvalinotti@icloud.com>
2021-06-09 10:46:45 -0400

    Production build + publish config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,15 @@
+import path from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    target: 'modules',
+    outDir: 'dist',
+    assetsDir: '',
+    sourcemap: process.env.NODE_ENV === 'production',
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'SoundPlayer'
+    }
+  },
+});
